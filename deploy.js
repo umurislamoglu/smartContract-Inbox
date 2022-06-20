de9955979a2b6fee859782e2ba6252c98ea81647
Umur İslamoğlu <74820307+umurislamoglu@users.noreply.github.com>
2022-06-21 00:44:49 +0300

    code refactoring

--- deploy.js
+++ deploy.js
@@ -1,7 +1,7 @@
 // deploy code will go here
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 const Web3 = require("web3");
-const { interface, bytecode } = require("./compile");
+const { abi, evm } = require('./compile');
 require("dotenv").config();
 
 let INITIAL_STRING = "Hi There!";
@@ -16,11 +16,9 @@ const web3 = new Web3(provider);
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
 
-  console.log(accounts[0]);
-
-  const result  = await new web3.eth.Contract(JSON.parse(interface))
+  const result  = await new web3.eth.Contract(abi)
     .deploy({
-      data: bytecode,
+      data: evm.bytecode.object,
       arguments: [INITIAL_STRING],
     })
     .send({
@@ -28,9 +26,6 @@ const deploy = async () => {
       gas: "1000000",
     });
 
-  console.log(result.options.address);
-
   provider.engine.stop()
-
 };
 deploy();
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,7 +2,7 @@
 const assert = require("assert");
 const ganache = require("ganache-cli");
 const Web3 = require("web3");
-const { interface, bytecode } = require("../compile");
+const { abi, evm } = require("../compile");
 const web3 = new Web3(ganache.provider());
 
 let accounts;
@@ -14,9 +14,9 @@ beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
 
   // use one of those accounts to deploy the contract
-  inbox = await new web3.eth.Contract(JSON.parse(interface))
+  inbox = await new web3.eth.Contract(abi)
     .deploy({
-      data: bytecode,
+      data: evm.bytecode.object,
       arguments: [INITIAL_STRING],
     })
     .send({
